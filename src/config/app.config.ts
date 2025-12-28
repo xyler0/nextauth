4b0ca60b99f38fbeac8d8c25487a0ef4a9f37ece
xyler0 <harjaserridwan@gmail.com>
2025-12-28 21:46:17 +0100

    config: add environment configuration structure

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,5 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: process.env.PORT || 3000,
   environment: process.env.NODE_ENV || 'development',
   maxPostsPerDay: parseInt(process.env.MAX_POSTS_PER_DAY, 10) || 3,
 });
