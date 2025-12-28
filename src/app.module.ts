2f0bffd9daf88b64b764e45d7a7828acac539391
xyler0 <harjaserridwan@gmail.com>
2025-12-28 21:48:28 +0100

    config: integrate ConfigModule globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+import githubConfig from './config/github.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, githubConfig],
+      envFilePath: ['.env'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
