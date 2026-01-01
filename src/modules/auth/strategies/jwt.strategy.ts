33de1ed1d356e61b35dbb281b55f21570177fcd4
xyler0 <harjaserridwan@gmail.com>
2026-01-01 22:39:04 +0100

    feat(auth): implement JWT strategy and CurrentUser decorator

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private config: ConfigService,
+    private auth: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get<string>('JWT_SECRET')!,
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.auth.validateUser(payload.sub);
+    
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user; // Attached to request.user
+  }
+}
