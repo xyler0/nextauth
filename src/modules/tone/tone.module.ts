43a15774a66ba80b00a237f4c54972e797f656fd
xyler0 <harjaserridwan@gmail.com>
2026-01-16 19:57:39 +0100

    refactor(tone): add PatternModule dependency

--- src/modules/tone/tone.module.ts
+++ src/modules/tone/tone.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ToneService } from './tone.service';
+import { PatternModule } from '../pattern/pattern.module';
 
 @Module({
+  imports: [PatternModule],
   providers: [ToneService],
   exports: [ToneService],
 })
