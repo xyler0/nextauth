22e006e384bab902d2438282c5d7e45144fd45be
xyler0 <harjaserridwan@gmail.com>
2026-01-20 22:09:08 +0100

    feat: extend nextauth types with custom fields

---
+++ auth/types/next-auth.d.ts
@@ -0,0 +1,26 @@
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      email: string;
+      name: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    email: string;
+    name: string | null;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    email: string;
+    name: string;
+    provider?: string;
+  }
+}
